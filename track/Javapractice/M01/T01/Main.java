36caa948030f01850a72da11558fc84ab9474692
pavithra <k.pavithra9573@gmail.com>
2026-08-03 16:04:48 +0530

    problem1

---
+++ track/Javapractice/M01/T01/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String LearnerName = "Ananya";
+        int completedProblems = 25;
+        double practiceHours = 2.5;
+        boolean projectCompleted = true;
+        System.out.println("Learner: " + LearnerName);
+        System.out.println("Problems Completed: " + completedProblems);
+        System.out.println("Practice Hours: " + practiceHours);
+        System.out.println("Project Completed: " + projectCompleted);
+    }
+}
