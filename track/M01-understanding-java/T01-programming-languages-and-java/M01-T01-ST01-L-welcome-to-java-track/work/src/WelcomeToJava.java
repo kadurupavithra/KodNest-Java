8d033eeb6efb749944669f1e4e5613492d3b3c4a
pavithra <k.pavithra9573@gmail.com>
2026-08-03 12:01:45 +0530

    main.java

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -4,6 +4,7 @@ public class WelcomeToJava {
         System.out.println(" I already know how to build programming logic.");
         System.out.println(" Now I am learning how to express that logic using java.");
         System.out.println(" I know how to use git and github");
+        System.out.println(" I know how to use git ");
        //Write your code here
     }
 }g
