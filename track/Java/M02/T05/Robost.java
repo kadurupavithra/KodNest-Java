65e2f3ba1ebf8556a7ad20152ed51be6b0a7cf34
pavithra <k.pavithra9573@gmail.com>
2026-08-11 15:41:27 +0530

    firstPush

---
+++ track/Java/M02/T05/Robost.java
@@ -0,0 +1,16 @@
+
+class Robot {
+    void speak() {
+        // Print the message
+        System.out.println("Beep beep! Java is my superpower!");
+    }
+}
+
+public class Robost {
+    public static void main(String[] args) {
+        Robot r = new Robot();
+
+        // Call speak()
+        r.speak();
+    }
+}
