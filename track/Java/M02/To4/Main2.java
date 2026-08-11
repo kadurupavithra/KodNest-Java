3eec4f75e8e997c8226afb3c7a892aebddf3b992
pavithra <k.pavithra9573@gmail.com>
2026-08-11 15:24:46 +0530

    firstpush

---
+++ track/Java/M02/To4/Main2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Student {
+    int id;
+    String name;
+    String course;
+    double javaScore;
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Student s1 = new Student();
+
+        s1.id = sc.nextInt();
+        sc.nextLine();
+        s1.name = sc.nextLine();
+        s1.course = sc.nextLine();
+        s1.javaScore = sc.nextDouble();
+
+        System.out.println("Student Profile");
+        System.out.println("ID: " + s1.id);
+        System.out.println("Name: " + s1.name);
+        System.out.println("Course: " + s1.course);
+        System.out.println("Java Score: " + s1.javaScore);
+
+        sc.close();
+    }
+}
