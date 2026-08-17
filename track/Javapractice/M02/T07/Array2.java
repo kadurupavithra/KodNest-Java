e1200992a4b050a433b5d1b627b3d3e599a1ebf4
pavithra <k.pavithra9573@gmail.com>
2026-08-17 12:49:28 +0530

    firstPush

--- track/Javapractice/M02/T07/Array2.java
+++ track/Javapractice/M02/T07/Array2.java
@@ -1,17 +1,21 @@
+import java.*;
 import java.util.Scanner;
 
-public class Array {
+public class Array2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a[] = new int[5];
+        System.out.println("enter the size of array");
+        int n = sc.nextInt();
+        int a[] = new int[n];
         System.out.println("Enter array elements");
         for (int i = 0; i <= a.length - 1; i++) {
             a[i] = sc.nextInt();
         }
         System.out.println("array elements are:");
-        for (int i = 0; i <= a.length - 1; i++) {
+        for (int i = a.length - 1; i >= 0; i--) {
             System.out.println(a[i]);
         }
+
     }
 
 }
